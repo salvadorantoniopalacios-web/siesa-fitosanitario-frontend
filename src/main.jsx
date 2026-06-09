89f8a2eff73e98041ca9093efa4b5fce5e01c890
Salvador Palacios <salvadorantoniopalacios@gmail.com>
2026-06-09 13:08:33 -0600

    feat: activar PWA y registro de service worker

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,25 @@ import App from "./App.jsx";
 
 import "./config/axiosConfig.js";
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((registration) => {
+        console.log(
+          "✅ Service Worker registrado:",
+          registration.scope
+        );
+      })
+      .catch((error) => {
+        console.error(
+          "❌ Error registrando Service Worker:",
+          error
+        );
+      });
+  });
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
